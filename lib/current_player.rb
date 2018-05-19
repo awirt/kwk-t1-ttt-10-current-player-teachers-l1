55de4782f21703b5d62922c31c958095fbf869ff
Andrew Wirt <awjwirt@gmail.com>
2018-05-19 17:10:21 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ def turn_count(board)
   counter = 0
   
   board.each do |space|
-    if (space != " " || space != "")
+    if space != " "
       puts counter
       counter += 1
       puts counter
